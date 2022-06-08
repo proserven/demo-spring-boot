e52caf62fef7a6d566984a23cd3f06129c176314
proserven <proserven@users.noreply.github.com>
2022-06-08 14:34:54 -0400

    Initial commit

---
+++ src/main/java/com/example/DemoSpringBootApplication.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringBootApplication.class, args);
+    }
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello from Spring Boot!";
+    }
+    
+}
